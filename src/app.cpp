e64bd4b7dc75034b580d6e7c2930cc61e21fa674
Eslam Linux <linuxeslam@gmail.com>
2025-09-28 21:34:21 +0300

    Create app.cpp

---
+++ src/app.cpp
@@ -0,0 +1,26 @@
+#include "ui/main_frame.h"
+#include <wx/app.h>
+#include <wx/image.h>
+#include <wx/cmdline.h>
+#include <wx/filename.h>
+#include <wx/stdpaths.h>
+#include <wx/dir.h>
+#include <wx/log.h>
+#include <wx/translation.h>
+#include <wx/splash.h>
+#include <wx/sysopt.h>
+#include <wx/msgdlg.h>
+#include <wx/config.h>
+#include <wx/dcmemory.h>  // إضافة هذا الرأس لـ wxMemoryDC
+#include <wx/brush.h>     // إضافة هذا الرأس لـ wxBrush
+
+class PhotoEditorApp : public wxApp {
+public:
+    virtual bool OnInit() override;
+    virtual int OnExit() override;
+    virtual void OnInitCmdLine(wxCmdLineParser& parser) override;
+    virtual bool OnCmdLineParsed(wxCmdLineParser& parser) override;
+    virtual bool OnExceptionInMainLoop() override;
+    virtual void OnUnhandledException() override;
+    virtual void OnFatalException() override;
+    
