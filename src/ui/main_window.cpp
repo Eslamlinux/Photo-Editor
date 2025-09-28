0d4a5275fb4faf812c9e8cb353cb0cc68f18bb0b
Eslam Linux <linuxeslam@gmail.com>
2025-09-28 21:48:02 +0300

    Update main_window.cpp

--- src/ui/main_window.cpp
+++ src/ui/main_window.cpp
@@ -29,3 +29,41 @@ MainWindow::MainWindow(const wxString& title)
     createStatusBar();
     createPanels();
     
+    // Set up AUI manager
+    m_auiManager.SetFlags(wxAUI_MGR_DEFAULT | wxAUI_MGR_LIVE_RESIZE);
+    
+    // Set icon
+    SetIcon(wxArtProvider::GetIcon(wxART_FRAME_ICON));
+    
+    // Bind events
+    Bind(wxEVT_CLOSE_WINDOW, &MainWindow::onClose, this);
+    
+    // Apply initial theme
+    applyTheme();
+    
+    // Set up AUI manager
+    m_auiManager.Update();
+    
+    // Center on screen
+    Centre();
+}
+
+MainWindow::~MainWindow() {
+    m_auiManager.UnInit();
+}
+
+void MainWindow::createMenuBar() {
+    wxMenuBar* menuBar = new wxMenuBar();
+    
+    // File menu
+    wxMenu* fileMenu = new wxMenu();
+    fileMenu->Append(wxID_NEW, _("&New Project\tCtrl+N"));
+    fileMenu->Append(wxID_OPEN, _("&Open Project\tCtrl+O"));
+    fileMenu->Append(wxID_SAVE, _("&Save Project\tCtrl+S"));
+    fileMenu->Append(wxID_SAVEAS, _("Save Project &As...\tCtrl+Shift+S"));
+    fileMenu->AppendSeparator();
+    fileMenu->Append(ID_IMPORT_IMAGE, _("&Import Image...\tCtrl+I"));
+    fileMenu->Append(ID_EXPORT_IMAGE, _("&Export Image...\tCtrl+E"));
+    fileMenu->AppendSeparator();
+    fileMenu->Append(wxID_EXIT, _("E&xit\tAlt+F4"));
+    
