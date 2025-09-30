6b447d7a65ee220ac362253953a0164f06ea12c3
Eslam Linux <linuxeslam@gmail.com>
2025-10-01 01:26:29 +0300

    Update main_frame.cpp

--- src/ui/main_frame.cpp
+++ src/ui/main_frame.cpp
@@ -827,3 +827,50 @@ void MainFrame::onSaturation(wxCommandEvent& event)
     // تعيين التركيز على لوحة الرسم
     m_canvasPanel->SetFocus();
 }
+
+void MainFrame::onHue(wxCommandEvent& event)
+{
+    // التحقق من وجود صورة
+    if (!m_imageProcessor->hasImage()) {
+        return;
+    }
+    
+    // إنشاء مربع حوار
+    wxDialog dialog(this, wxID_ANY, _("Hue"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE);
+    
+    // إنشاء عناصر مربع الحوار
+    wxSlider* slider = new wxSlider(&dialog, wxID_ANY, 0, -180, 180, wxDefaultPosition, wxSize(300, -1));
+    wxStaticText* valueText = new wxStaticText(&dialog, wxID_ANY, "0");
+    wxButton* okButton = new wxButton(&dialog, wxID_OK, _("OK"));
+    wxButton* cancelButton = new wxButton(&dialog, wxID_CANCEL, _("Cancel"));
+    
+    // ربط حدث تغيير المنزلق
+    slider->Bind(wxEVT_SLIDER, [slider, valueText](wxCommandEvent&) {
+        valueText->SetLabel(wxString::Format("%d", slider->GetValue()));
+    });
+    
+    // إنشاء السايزر
+    wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+    buttonSizer->Add(okButton, 0, wxALL, 5);
+    buttonSizer->Add(cancelButton, 0, wxALL, 5);
+    
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    mainSizer->Add(new wxStaticText(&dialog, wxID_ANY, _("Adjust hue:")), 0, wxALL, 5);
+    mainSizer->Add(slider, 0, wxEXPAND | wxALL, 5);
+    mainSizer->Add(valueText, 0, wxALIGN_CENTER | wxALL, 5);
+    mainSizer->Add(new wxStaticLine(&dialog, wxID_ANY), 0, wxEXPAND | wxALL, 5);
+    mainSizer->Add(buttonSizer, 0, wxALIGN_CENTER | wxALL, 5);
+    
+    // تعيين السايزر
+    dialog.SetSizer(mainSizer);
+    mainSizer->Fit(&dialog);
+    
+    // عرض مربع الحوار
+    if (dialog.ShowModal() == wxID_OK) {
+        // تطبيق تدرج اللون
+        m_imageProcessor->adjustHue(slider->GetValue());
+    }
+    
+    // تعيين التركيز على لوحة الرسم
+    m_canvasPanel->SetFocus();
+}
