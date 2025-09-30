0264bf911143e9f67cc55f67f276359dd83d6e98
Eslam Linux <linuxeslam@gmail.com>
2025-10-01 00:49:43 +0300

    Create main_frame.h

---
+++ src/ui/main_frame.h
@@ -0,0 +1,18 @@
+#ifndef PME_UI_MAIN_FRAME_H
+#define PME_UI_MAIN_FRAME_H
+
+#include <wx/frame.h>
+#include <wx/string.h>
+#include <wx/menu.h>
+#include <wx/toolbar.h>
+#include <wx/statusbr.h>
+
+namespace pme {
+
+namespace core {
+class ImageProcessor;
+}
+
+namespace ui {
+
+#endif // PME_UI_MAIN_FRAME_H
