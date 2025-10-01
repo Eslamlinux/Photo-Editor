9930f0465c0720ac096d48a24336b2ea1e3932d2
Eslam Linux <linuxeslam@gmail.com>
2025-10-02 01:36:46 +0300

    Update image_processor.cpp

--- src/core/image_processor.cpp
+++ src/core/image_processor.cpp
@@ -20,3 +20,27 @@ void ImageProcessor::setUpdateCallback(UpdateCallback callback)
 {
     m_updateCallback = callback;
 }
+
+bool ImageProcessor::loadImage(const std::string& filePath)
+{
+    // محاولة تحميل الصورة
+    cv::Mat image = cv::imread(filePath, cv::IMREAD_UNCHANGED);
+    
+    // التحقق من نجاح التحميل
+    if (image.empty()) {
+        return false;
+    }
+    
+    // تعيين الصورة الحالية
+    m_image = image;
+    m_originalImage = image.clone();
+    
+    // مسح سجلات التراجع/الإعادة
+    m_undoStack.clear();
+    m_redoStack.clear();
+    
+    // إشعار بالتحديث
+    notifyUpdate();
+    
+    return true;
+}
