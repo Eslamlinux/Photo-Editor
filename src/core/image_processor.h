0463bac865a11042bded784fc00606a0b2724bfc
Eslam Linux <linuxeslam@gmail.com>
2025-10-04 00:22:33 +0300

    Create image_processor.h

---
+++ src/core/image_processor.h
@@ -0,0 +1,13 @@
+#ifndef PME_CORE_IMAGE_PROCESSOR_H
+#define PME_CORE_IMAGE_PROCESSOR_H
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <opencv2/core.hpp>
+
+namespace pme {
+namespace core {
+
+
+#endif // PME_CORE_IMAGE_PROCESSOR_H
